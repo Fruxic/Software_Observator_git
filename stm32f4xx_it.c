f42263e9557ff26567299a0d8cc8906ed34ce191
Michael Gallagher <Michael_galcobouw@live.com>
2020-01-30 12:23:01 +0100

    Added usage of internal sensor

--- stm32f4xx_it.c
+++ stm32f4xx_it.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    stm32f4xx_it.c
-  * @brief   Interrupt Service Routines.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    stm32f4xx_it.c
+ * @brief   Interrupt Service Routines.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -67,6 +67,7 @@ char FileName[] = "Measure.txt";//Bestands naam voor externe sensor
 extern SD_HandleTypeDef hsd;
 extern TIM_HandleTypeDef htim2;
 extern DMA_HandleTypeDef hdma_usart2_rx;
+extern UART_HandleTypeDef huart6;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -216,124 +217,124 @@ void EXTI1_IRQHandler(void)
   /* USER CODE BEGIN EXTI1_IRQn 0 */
 	if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_1) != RESET)
 	{
-	  __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_1);
-	  static int State = 1;
-	  static int Reading;
-	  static int Previous = 0;
-	  static int VorigSec = 0;
-	  static int b = 0;
-	  static int Calc[10];
-	  char Tekst[] = "Regen input 1:\r\n";
-	  char Tekst1[] = "mm/h\r\n";
-	  char Tekst2[] = "Tijd:\t";
-	  char Tekst3[] = ":";
-	  char Tekst4[] = "\r\n\r\n";
+		__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_1);
+		static int State = 1;
+		static int Reading;
+		static int Previous = 0;
+		static int VorigSec = 0;
+		static int b = 0;
+		static int Calc[10];
+		char Tekst[] = "Regen input 1:\r\n";
+		char Tekst1[] = "mm/h\r\n";
+		char Tekst2[] = "Tijd:\t";
+		char Tekst3[] = ":";
+		char Tekst4[] = "\r\n\r\n";
 
-	  Reading++;
+		Reading++;
 
-	  if(Reading == 1 && Previous == 0)
-	  {
-		  if(State == 1)
-		  {
-			  State = 0;
-			  GPIOC -> ODR ^= GPIO_PIN_11;
-			  HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
-			  HAL_RTC_GetDate(&hrtc, &sDate, RTC_FORMAT_BIN);
-			  int HuidigSec = sTime.Seconds;
-			  int x = 0;
-			  int Sum = 0;
-			  float Avg = 0;
+		if(Reading == 1 && Previous == 0)
+		{
+			if(State == 1)
+			{
+				State = 0;
+				GPIOC -> ODR ^= GPIO_PIN_11;
+				HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
+				HAL_RTC_GetDate(&hrtc, &sDate, RTC_FORMAT_BIN);
+				int HuidigSec = sTime.Seconds;
+				int x = 0;
+				int Sum = 0;
+				float Avg = 0;
 
-			  if(VorigSec < HuidigSec)
-			  {
-				  x = HuidigSec - VorigSec;
-			  }
-			  else if(VorigSec > HuidigSec)
-			  {
-				  x = (HuidigSec + 60) - VorigSec;
-			  }
+				if(VorigSec < HuidigSec)
+				{
+					x = HuidigSec - VorigSec;
+				}
+				else if(VorigSec > HuidigSec)
+				{
+					x = (HuidigSec + 60) - VorigSec;
+				}
 
-			  VorigSec = HuidigSec;
+				VorigSec = HuidigSec;
 
-			  if(x >= 1)
-			  {
-				  Calc[b] = (3600/x) * 0.1;
-				  b++;
-			  }
+				if(x >= 1)
+				{
+					Calc[b] = (3600/x) * 0.1;
+					b++;
+				}
 
-			  if(b == 10)
-			  {
-				  b = 0;
-				  for(int i = 0; i <= 9; i++)
-				  {
-					  Sum += Calc[i];
-				  }
-				  Avg = Sum/10;
-				  char DataH[3];
-				  itoa(Avg, DataH, 10);
-				  if(f_mount(&myFATFS, SDPath, 1) == FR_OK)
-				  {
-					  GPIOC -> ODR &= ~GPIO_PIN_9;
-					  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-					  f_write(&myFile, &Tekst, sizeof(Tekst), &testByte);
-					  f_close(&myFile);
+				if(b == 10)
+				{
+					b = 0;
+					for(int i = 0; i <= 9; i++)
+					{
+						Sum += Calc[i];
+					}
+					Avg = Sum/10;
+					char DataH[3];
+					itoa(Avg, DataH, 10);
+					if(f_mount(&myFATFS, SDPath, 1) == FR_OK)
+					{
+						GPIOC -> ODR &= ~GPIO_PIN_9;
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst, sizeof(Tekst), &testByte);
+						f_close(&myFile);
 
-					  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-					  f_write(&myFile, &DataH, sizeof(DataH), &testByte);
-					  f_close(&myFile);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &DataH, sizeof(DataH), &testByte);
+						f_close(&myFile);
 
-					  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-					  f_write(&myFile, &Tekst1, sizeof(Tekst1), &testByte);
-					  f_close(&myFile);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst1, sizeof(Tekst1), &testByte);
+						f_close(&myFile);
 
-					  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-					  f_write(&myFile, &Tekst2, sizeof(Tekst2), &testByte);
-					  f_close(&myFile);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst2, sizeof(Tekst2), &testByte);
+						f_close(&myFile);
 
-					  uint8_t h = sTime.Hours;
-					  char DataHours[2];
-					  itoa(h, DataHours, 10);
-					  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-					  f_write(&myFile, &DataHours, sizeof(DataHours), &testByte);
-					  f_close(&myFile);
+						uint8_t h = sTime.Hours;
+						char DataHours[2];
+						itoa(h, DataHours, 10);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &DataHours, sizeof(DataHours), &testByte);
+						f_close(&myFile);
 
-					  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-					  f_write(&myFile, &Tekst3, sizeof(Tekst3), &testByte);
-					  f_close(&myFile);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst3, sizeof(Tekst3), &testByte);
+						f_close(&myFile);
 
-					  uint8_t m = sTime.Minutes;
-					  char DataM[2];
-					  itoa(m, DataM, 10);
-					  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-					  f_write(&myFile, &DataM, sizeof(DataM), &testByte);
-					  f_close(&myFile);
+						uint8_t m = sTime.Minutes;
+						char DataM[2];
+						itoa(m, DataM, 10);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &DataM, sizeof(DataM), &testByte);
+						f_close(&myFile);
 
-					  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-					  f_write(&myFile, &Tekst3, sizeof(Tekst3), &testByte);
-					  f_close(&myFile);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst3, sizeof(Tekst3), &testByte);
+						f_close(&myFile);
 
-					  uint8_t s = sTime.Seconds;
-					  char DataS[2];
-					  itoa(s, DataS, 10);
-					  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-					  f_write(&myFile, &DataS, sizeof(DataS), &testByte);
-					  f_close(&myFile);
+						uint8_t s = sTime.Seconds;
+						char DataS[2];
+						itoa(s, DataS, 10);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &DataS, sizeof(DataS), &testByte);
+						f_close(&myFile);
 
-					  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-					  f_write(&myFile, &Tekst4, sizeof(Tekst4), &testByte);
-					  f_close(&myFile);
-					  GPIOC -> ODR |= GPIO_PIN_9;
-				  }
-			  }
-			  Reading = 0;
-		  }
-		  else
-		  {
-			  State = 1;
-			  Reading = 0;
-		  }
-	  }
-	  for(uint32_t i = 0; i<=100000; i++);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst4, sizeof(Tekst4), &testByte);
+						f_close(&myFile);
+						GPIOC -> ODR |= GPIO_PIN_9;
+					}
+				}
+				Reading = 0;
+			}
+			else
+			{
+				State = 1;
+				Reading = 0;
+			}
+		}
+		for(uint32_t i = 0; i<=100000; i++);
 	}
   /* USER CODE END EXTI1_IRQn 0 */
   HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_1);
@@ -348,125 +349,125 @@ void EXTI1_IRQHandler(void)
 void EXTI2_IRQHandler(void)
 {
   /* USER CODE BEGIN EXTI2_IRQn 0 */
-	    if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_2) != RESET)
+	if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_2) != RESET)
+	{
+		__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_2);
+		static int State = 1;
+		static int Reading;
+		static int Previous = 0;
+		static int VorigSec = 0;
+		static int b = 0;
+		static int Calc[10];
+		char Tekst[] = "Regen input 2:\r\n";
+		char Tekst1[] = "mm/h\r\n";
+		char Tekst2[] = "Tijd:\t";
+		char Tekst3[] = ":";
+		char Tekst4[] = "\r\n\r\n";
+
+		Reading++;
+
+		if(Reading == 1 && Previous == 0)
 		{
-		  __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_2);
-		  static int State = 1;
-		  static int Reading;
-		  static int Previous = 0;
-		  static int VorigSec = 0;
-		  static int b = 0;
-		  static int Calc[10];
-		  char Tekst[] = "Regen input 2:\r\n";
-		  char Tekst1[] = "mm/h\r\n";
-		  char Tekst2[] = "Tijd:\t";
-		  char Tekst3[] = ":";
-		  char Tekst4[] = "\r\n\r\n";
+			if(State == 1)
+			{
+				State = 0;
+				GPIOC -> ODR ^= GPIO_PIN_11;
+				HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
+				HAL_RTC_GetDate(&hrtc, &sDate, RTC_FORMAT_BIN);
+				int HuidigSec = sTime.Seconds;
+				int x = 0;
+				int Sum = 0;
+				float Avg = 0;
 
-		  Reading++;
+				if(VorigSec < HuidigSec)
+				{
+					x = HuidigSec - VorigSec;
+				}
+				else if(VorigSec > HuidigSec)
+				{
+					x = (HuidigSec + 60) - VorigSec;
+				}
 
-		  if(Reading == 1 && Previous == 0)
-		  {
-			  if(State == 1)
-			  {
-				  State = 0;
-				  GPIOC -> ODR ^= GPIO_PIN_11;
-				  HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
-				  HAL_RTC_GetDate(&hrtc, &sDate, RTC_FORMAT_BIN);
-				  int HuidigSec = sTime.Seconds;
-				  int x = 0;
-				  int Sum = 0;
-				  float Avg = 0;
+				VorigSec = HuidigSec;
 
-				  if(VorigSec < HuidigSec)
-				  {
-					  x = HuidigSec - VorigSec;
-				  }
-				  else if(VorigSec > HuidigSec)
-				  {
-					  x = (HuidigSec + 60) - VorigSec;
-				  }
+				if(x >= 1)
+				{
+					Calc[b] = (3600/x) * 0.1;
+					b++;
+				}
 
-				  VorigSec = HuidigSec;
+				if(b == 10)
+				{
+					b = 0;
+					for(int i = 0; i <= 9; i++)
+					{
+						Sum += Calc[i];
+					}
+					Avg = Sum/10;
+					char DataH[3];
+					itoa(Avg, DataH, 10);
+					if(f_mount(&myFATFS, SDPath, 1) == FR_OK)
+					{
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst, sizeof(Tekst), &testByte);
+						f_close(&myFile);
 
-				  if(x >= 1)
-				  {
-					  Calc[b] = (3600/x) * 0.1;
-					  b++;
-				  }
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &DataH, sizeof(DataH), &testByte);
+						f_close(&myFile);
 
-				  if(b == 10)
-				  {
-					  b = 0;
-					  for(int i = 0; i <= 9; i++)
-					  {
-						  Sum += Calc[i];
-					  }
-					  Avg = Sum/10;
-					  char DataH[3];
-					  itoa(Avg, DataH, 10);
-					  if(f_mount(&myFATFS, SDPath, 1) == FR_OK)
-					  {
-						  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-						  f_write(&myFile, &Tekst, sizeof(Tekst), &testByte);
-						  f_close(&myFile);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst1, sizeof(Tekst1), &testByte);
+						f_close(&myFile);
 
-						  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-						  f_write(&myFile, &DataH, sizeof(DataH), &testByte);
-						  f_close(&myFile);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst2, sizeof(Tekst2), &testByte);
+						f_close(&myFile);
 
-						  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-						  f_write(&myFile, &Tekst1, sizeof(Tekst1), &testByte);
-						  f_close(&myFile);
+						uint8_t h = sTime.Hours;
+						char DataHours[2];
+						itoa(h, DataHours, 10);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &DataHours, sizeof(DataHours), &testByte);
+						f_close(&myFile);
 
-						  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-						  f_write(&myFile, &Tekst2, sizeof(Tekst2), &testByte);
-						  f_close(&myFile);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst3, sizeof(Tekst3), &testByte);
+						f_close(&myFile);
 
-						  uint8_t h = sTime.Hours;
-						  char DataHours[2];
-						  itoa(h, DataHours, 10);
-						  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-						  f_write(&myFile, &DataHours, sizeof(DataHours), &testByte);
-						  f_close(&myFile);
+						uint8_t m = sTime.Minutes;
+						char DataM[2];
+						itoa(m, DataM, 10);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &DataM, sizeof(DataM), &testByte);
+						f_close(&myFile);
 
-						  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-						  f_write(&myFile, &Tekst3, sizeof(Tekst3), &testByte);
-						  f_close(&myFile);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst3, sizeof(Tekst3), &testByte);
+						f_close(&myFile);
 
-						  uint8_t m = sTime.Minutes;
-						  char DataM[2];
-						  itoa(m, DataM, 10);
-						  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-						  f_write(&myFile, &DataM, sizeof(DataM), &testByte);
-						  f_close(&myFile);
+						uint8_t s = sTime.Seconds;
+						char DataS[2];
+						itoa(s, DataS, 10);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &DataS, sizeof(DataS), &testByte);
+						f_close(&myFile);
 
-						  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-						  f_write(&myFile, &Tekst3, sizeof(Tekst3), &testByte);
-						  f_close(&myFile);
-
-						  uint8_t s = sTime.Seconds;
-						  char DataS[2];
-						  itoa(s, DataS, 10);
-						  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-						  f_write(&myFile, &DataS, sizeof(DataS), &testByte);
-						  f_close(&myFile);
-
-						  f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
-						  f_write(&myFile, &Tekst4, sizeof(Tekst4), &testByte);
-						  f_close(&myFile);
-					  }
-				  }
-				  Reading = 0;
-			  }
-			  else
-			  {
-				  State = 1;
-				  Reading = 0;
-			  }
-		  }
-		  for(uint32_t i = 0; i<=100000; i++);
+						f_open(&myFile, FileName, FA_WRITE | FA_OPEN_APPEND);
+						f_write(&myFile, &Tekst4, sizeof(Tekst4), &testByte);
+						f_close(&myFile);
+					}
+				}
+				Reading = 0;
+			}
+			else
+			{
+				State = 1;
+				Reading = 0;
+			}
 		}
+		for(uint32_t i = 0; i<=100000; i++);
+	}
   /* USER CODE END EXTI2_IRQn 0 */
   HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
   /* USER CODE BEGIN EXTI2_IRQn 1 */
@@ -514,6 +515,20 @@ void SDIO_IRQHandler(void)
   /* USER CODE BEGIN SDIO_IRQn 1 */
 
   /* USER CODE END SDIO_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART6 global interrupt.
+  */
+void USART6_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART6_IRQn 0 */
+
+  /* USER CODE END USART6_IRQn 0 */
+  HAL_UART_IRQHandler(&huart6);
+  /* USER CODE BEGIN USART6_IRQn 1 */
+
+  /* USER CODE END USART6_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
